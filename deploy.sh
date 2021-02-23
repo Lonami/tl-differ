14b0ebec7045b43c80ba3acb158d561c9c84141a
Lonami Exo <totufals@hotmail.com>
2021-02-23 19:23:38 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 set -e
 TMP=/tmp/tldiff
-mkdir $TMP
-cp app.js atom.xml diff.js index.html $TMP
+rm -rf "$TMP"
+mkdir "$TMP"
+cp app.js atom.xml diff.js index.html "$TMP"
 git checkout gh-pages
-mv $TMP/* .
+mv "$TMP"/* .
 git commit -am "Deploy new layer"
 git push
 git checkout master
