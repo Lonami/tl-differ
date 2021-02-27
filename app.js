06c92b19ab7a34b4aa2132c0fc523b7626159aaf
Lonami Exo <totufals@hotmail.com>
2021-02-27 16:03:55 +0100

    Use the minus sign to format the layer date

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ function layer_name(layer) {
     const year = date.getFullYear();
     const month = `0${date.getMonth() + 1}`.slice(-2);
     const day = `0${date.getDate()}`.slice(-2);
-    return `[${year}/${month}/${day}] ${name}`;
+    return `[${year}-${month}-${day}] ${name}`;
 }
 
 function toggle_expand(event) {
