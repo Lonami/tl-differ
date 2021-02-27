6cfdc56c7f3b2a6e0fb9ba0a54d1d27d62141a91
Lonami Exo <totufals@hotmail.com>
2021-02-27 16:03:33 +0100

    Remove unnecessary dict-like access

--- app.js
+++ app.js
@@ -49,8 +49,8 @@ function change_used_diff(event) {
 }
 
 function layer_name(layer) {
-    const name = layer['layer'] == null ? 'Unknown layer' : `Layer ${layer['layer']}`;
-    const date = new Date(layer['date'] * 1000);
+    const name = layer.layer == null ? 'Unknown layer' : `Layer ${layer.layer}`;
+    const date = new Date(layer.date * 1000);
     const year = date.getFullYear();
     const month = `0${date.getMonth() + 1}`.slice(-2);
     const day = `0${date.getDate()}`.slice(-2);
